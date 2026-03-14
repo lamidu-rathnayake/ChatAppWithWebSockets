839f8713905a4d11448cb125f7836121821d3e8d
lamidu rathnayake <lamidurathnayakeofficial@gmail.com>
2026-03-14 18:26:35 +0530

    i added hte final version of the backend server with websocket powered by fastAPI

--- websocket-server/client.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import websockets
-
-async def test():
-    async with websockets.connect("ws://localhost:8001") as ws:
-        await ws.send("hello")
-        print("sent")
-
-asyncio.run(test())
--- websocket-server/server.py
+++ websocket-server/server.py
@@ -1,26 +1,64 @@
-import asyncio
-import websockets
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import uvicorn
+from collections import defaultdict
 
-# Function to handle each client connection
-async def handle_client(websocket, path):
-    # Add the new client to the set of connected clients
+
+class SocketManager:
+    def __init__(self):
+        self.rooms = defaultdict(list)
+        self.clientNames = defaultdict(str)
+
+    async def connect(self, websocket: WebSocket, roomId: str, clientName: str):
+        self.rooms[roomId].append(websocket) # apending the new client to the client list of the room id 
+        self.clientNames[websocket] = clientName # adding the new name record to the dictionary
+    
+    async def disconnect(self, websocket: WebSocket, roomId: str):
+        if websocket in self.rooms[roomId]:
+            self.rooms[roomId].remove(websocket)
+        
+        if websocket in self.clientNames:
+            del self.clientNames[websocket]
+
+        if not self.rooms[roomId]:
+            del self.rooms[roomId]
+
+    async def roomBroadcast(self, websocket: WebSocket, roomId: str, message: str):
+        jsonMessage = {
+            "sender": self.clientNames.get(websocket),
+            "msg": message 
+        }
+
+        for connection in self.rooms[roomId]:
+            if connection == WebSocket:
+                pass
+            else:
+                await connection.send_json(jsonMessage)
+
+
+
+
+app = FastAPI()
+socketManager = SocketManager()
+
+@app.websocket("/ws/{roomId}/{name}")
+async def websocket_endpoint(websocket: WebSocket, roomId: str, name: str):
+    await websocket.accept()
+    await socketManager.connect(websocket, roomId, name)
     
     try:
-        # Listen for messages from the client
-        message = await websocket.recv()
-        print(message)
+        while True:
+            data = await websocket.receive_text()
+            await socketManager.roomBroadcast(websocket, roomId, data)
+            
+    except WebSocketDisconnect:
+        print(f"Client {websocket.client.port} disconnected.")
+        await socketManager.disconnect(websocket,roomId)
         
-    except websockets.exceptions.ConnectionClosed:
-        pass
+    except Exception as ex:
+        print(f"Exception: {str(ex)}")
+        await socketManager.disconnect(websocket, roomId)
+        await websocket.close()
 
-    finally:
-        pass
 
-# Main function to start the WebSocket server
-async def main():
-    server = await websockets.serve(handle_client, 'localhost', 8080)
-    await server.wait_closed()
-
-# Run the server
 if __name__ == "__main__":
-    asyncio.run(main())
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
